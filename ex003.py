cb116edbb4029fd0dc55f5322b51e8545568cd6b
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-10 16:50:19 -0300

    ex 003
    Crie um programa que leia dois números e mostre a soma entre eles.

---
+++ ex003.py
@@ -0,0 +1,24 @@
+'''
+soma entre 2 valores
+print 'A soma entre (n) e (n) é igual a
+'''
+'''
+valor_um = int(input('Digite um valor'))
+valor_dois = int(input('Digite outro valo'))
+resultado = valor_um + valor_dois
+print('A soma entre', valor_um, 'e', valor_dois, 'é igual a', resultado)
+'''
+
+'''
+TIPOS PRIMITIVOS 
+
+INT(São valores inteiros ex. 0 ,-1,5,10)
+FLOAT(São valores quebrados ex. 2.44 , 0.44, -4.55, 7.0)
+BOOL(Boleanos True , False)
+STR(Para palavras ex. 'olà mundo!')
+PRINT('A soma vale{}'.format(s))
+'''
+n1 = int(input('Digite um valor '))
+n2 = int(input('Digite o valor que quer somar '))
+r = n1 + n2
+print('O valor entre {} e {} será de {}!'.format(n1, n2, r))
