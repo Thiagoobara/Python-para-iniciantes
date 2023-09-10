46646c50a30724f270e5e89d643f5e6ec83a3283
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-10 16:54:55 -0300

    Update ex004.py

---
+++ ex004.py
@@ -0,0 +1,18 @@
+'''
+Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as inf possíveis sobre ele.
+'''
+'''dg1 = input('Digite algo ')
+print(dg1.isnumeric())'''
+''''
+dg1 = input('Digite algo ')
+print(dg1.isalpha())
+'''
+dg1 =input('Digite algo ')
+print('O tipo primitivo desse valor é ',type(dg1))
+print('Só tem espaços?',dg1.isspace())
+print('Ele é um numero ?',dg1.isnumeric())
+print('Ele é alfabético ? ',dg1.isalpha())
+print('Ele é alfanumérico ?',dg1.isalnum())
+print('Está em maiúscula ?',dg1.isupper())
+print('Está só em minúscula ?',dg1.islower())
+print('Está capitalizada ?', dg1.istitle())
