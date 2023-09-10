5d158c8cf1ecc8a0552296208f1b285b02c54b00
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-10 17:25:38 -0300

    ex005
    Faça um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.

---
+++ ex005.py
@@ -0,0 +1,7 @@
+'''
+Faça um programa que leia um numero inteiro e mostre na tela seu sucessor e seu antecessor.
+'''
+n1 = int(input('Digite um numero '))
+su = n1 + 1
+an = n1 - 1
+print('O sucessor será {}, e seu antecessor {}'.format(su , an))
