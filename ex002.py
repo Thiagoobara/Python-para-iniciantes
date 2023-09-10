e64fff59e2eeccd7175220e0206aba3c30e765ab
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-10 16:47:18 -0300

    ex002
    Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.

---
+++ ex002.py
@@ -0,0 +1,11 @@
+'''
+nome = input('Digite seu nome')
+print('E um prazer te conhecer,', nome, ('!'))
+'''
+
+nome = input('Digite seu nome ')
+print('É um prazer te conhecer, {}!'.format(nome))
+
+'''
+OBS: QUANDO SE ABRE A {} SIGNIFICA QUE ALGO PODE SER INSERIDO , O TEXTO CONTINUA NORMAL, E PARA FINALIZAR .FORMAT(A VARIAVEL)
+'''
