28c32f6a16194f5a589fd713e0d4decf6768ab0b
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-11 16:59:54 -0300

    df009.py
    Faça um programa que leia um numero inteiro qualquer e mostre sua tabuada

---
+++ df009.py
@@ -0,0 +1,36 @@
+'''
+Faça um programa que leia um numero inteiro qualquer e mostre sua tabuada
+'''
+n1 = int(input('Digite um numero '))
+ro = ('-'*12)
+print('{}\n{} x 1 = {}\n{} x 2 = {}\n{} x 3 = {}\n{} x 4 = {}\n{} x 5 = {}\n{} x 6 = {}\n{} x 7 = {}\n{} x 8 = {}\n{} x 9 = {}\n{} x 10 = {}'.format(ro, n1, (n1*1), n1, (n1*2), n1, (n1*3), n1, (n1*4), n1, (n1*5), n1, (n1*6), n1, (n1*7), n1, (n1*8), n1, (n1*9), n1, (n1*10), (ro)))
+      
+
+
+
+
+'''
+n01 = n1 * 1
+n02 = n1 * 2
+n03 = n1 * 3
+n04 = n1 * 4
+n05 = n1 * 5
+n06 = n1 * 6
+n07 = n1 * 7
+n08 = n1 * 8
+n09 = n1 * 9
+n10 = n1 * 10
+print('Resultado será ')
+print('=*20')
+print('1x',=n01)
+print('2x',=n02)
+print('3x',=n03)
+print('4x',=n04)
+print('5x',n05)
+print('6x',n06)
+print('7x',n07)
+print('8x',n08)
+print('9x',n09)
+print('10x',n10)
+print('=*20')
+'''
