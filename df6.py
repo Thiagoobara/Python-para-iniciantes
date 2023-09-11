0bc9cd5cce121adb915df7618a2e8ae445cc3f36
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-11 09:34:36 -0300

    Desafio 06
    Crie um algoritimo que leia um número e mostre o seu dobro, triplo e sua raiz quadrada.

---
+++ df6.py
@@ -0,0 +1,14 @@
+'''
+Crie um algoritimo que leia um número e mostre o seu dobro, triplo e sua raiz quadrada.
+'''
+'''
+n1 = int(input('Digite um numero '))
+d = n1 * 2
+t = n1 * 3
+r = n1 **(1/2)
+print('O dobro sera de {},\n o triplo sera de {},\n e sua raiz quadrada {:.3f}'.format(d, t, r))
+'''
+n = int(input('Digite um numero '))
+print('O dobro será de {}.'.format((n*2)))
+print('O triplo será de {},\nE sua raiz quadrada {:.2f}'.format((n*3),  (n**(1/2))))
+
