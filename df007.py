3d8343ac118b83b3bc6271a12eb98ee467ecba44
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-11 16:54:59 -0300

    df007.py
    Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.

---
+++ df007.py
@@ -0,0 +1,7 @@
+'''
+Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+'''
+n1 = float(input('Digite sua 1° nota '))
+n2 = float(input('Digite sua 2° nota '))
+m = (n1 + n2) / 2
+print('Sua média é {:.2f}'.format(m))
