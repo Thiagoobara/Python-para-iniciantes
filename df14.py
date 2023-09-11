72bba2d0ee0a85ee0537e3d276073eccbcdf3f7b
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-11 16:33:34 -0300

    df14.py
    Escreva um programa que converta um temperatura digitada em °C e converta em °F.

---
+++ df14.py
@@ -0,0 +1,6 @@
+'''
+Escreva um programa que converta um temperatura digitada em °C e converta em °F
+'''
+t = float(input('Digite a temperatura em °C '))
+f = ((9 * t)/ 5 )+32
+print('A temperatura de {}°C corresponde a {}°F.'.format(t, f))
