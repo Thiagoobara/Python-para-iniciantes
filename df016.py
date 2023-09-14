75b49904ee4cc878ac3713c9d71e448e6d67b52f
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-14 07:13:34 -0300

    df016.py
    Módulos

---
+++ df016.py
@@ -0,0 +1,57 @@
+'''
+Módulos
+ex: matc Matemática 
+matc
+ceil = aredonda p cima
+floor = aredonda p baixo
+trunc = alimina da , para frente  
+pow = potência
+sqrt = raiz quadrada
+factorial = fatorial
+
+ex
+import math vai importar toda a math
+import math sqrt
+'''
+'''
+Exemplo 1
+import math
+n = float(input('Digite um numero '))
+r = math.sqrt(n)
+print('Raiz quadrada é de {}'.format(math.ceil (r)))
+'''
+'''
+Exemplo 2 importando 1
+from math import sqrt
+n = float(input('Digite um numero '))
+r = sqrt(n)
+print('Raiz quadrada é de {:.2f}'.format(r))
+'''
+'''
+Exemplo 2 importando 2
+from math import sqrt , floor
+n = float(input('Digite um numero '))
+r = sqrt(n)
+print('Raiz quadrada é de {:.2f}'.format((floor(r))))
+'''
+''''
+Exemplo 3 random e randint
+import random
+nu = random.randint(1, 10 )
+print(nu)
+'''
+
+'''
+import random
+nu = random.randint(1, 10 )
+print(nu)
+'''
+'''
+import emoji
+print( 'Olá, Mundo :\U0001f596', use_aliases=True'))
+'''
+import emoji
+
+print(emoji.emojize(":grinning_face_with_big_eyes:"))
+                    
+
