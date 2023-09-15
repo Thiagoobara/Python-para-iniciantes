e8eb42f5da06e5edd8275f788f49fbca3f29d43d
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-15 19:22:25 -0300

    df018.py
    Faça um programa que leia um ângulo qualquer na tela o valor do seno, cosseno e tangente desse ângulo

--- df018.py
+++ df018.py
@@ -1,3 +1,11 @@
 '''
 Faça um programa que leia um angulo qualquer na tela o valor do seno, cosseno e tangente desse ângulo
 '''
+
+from math import sin, radians, cos, tan
+an = float(input('Digite o ângulo: '))
+se = sin(radians(an))
+co = cos(radians(an))
+tg = tan(radians(an))
+print('O ângulo de {}°\nTem o seno de {:.2f}\nSeu cosseno é de {:.2f}\nE sua tangente {:.2f}'.format(an, se, co, tg))
+
