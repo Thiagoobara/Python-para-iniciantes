fb9ea81523e2da00a2a2b327756601c288e8bf40
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-15 19:00:56 -0300

    df017.py
    Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo, calcule e mostre o comprimento da hipotenusa

--- df017.py
+++ df017.py
@@ -2,12 +2,13 @@
 Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa
 '''
 # c2=a2+b2.
-from math import pow, sqrt
+from math import hypot
 ct = float(input('Digite o valor do cateto oposto '))
 ca = float(input('Digite o valor do cateto adjacente '))
-r = pow(2.0,ct) + pow(2.0,ca)
-c = sqrt(r)
-print('Comprimento da hipotenusa séra de {:.2f}'.format(c))
+r = hypot(ct, ca)
+print('Comprimento da hipotenusa séra de {:.2f}'.format(r))
+
+#obs: hypot calcula a hipotenusa
 
 
 
