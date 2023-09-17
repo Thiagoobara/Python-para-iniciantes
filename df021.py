6ad53465afa3d6719f1267b1d291318912ee7f79
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-17 10:29:04 -0300

    df021.py
    Faça um programa em Python que abra e reproduza o áudio em MP3.

--- df021.py
+++ df021.py
@@ -1,3 +1,20 @@
 '''
 Faça um programa em python que abra e reproduza o áudio em MP3.
+Obs: pygame.init() init para iniciar o pygame
+Obs: pygame.mixer.music.load('m001.mp3') para importar p arquivo
+     pygame.mixer.music.play() para tocar a musica
+     pygame.event.wait() para parar a musica 
+
 '''
+import pygame
+pygame.init()
+pygame.mixer.music.load('m001.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+
+
+
+
+
