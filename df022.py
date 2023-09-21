2fccf7d4865ee0efcf9854aa42a642f8aa72429c
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-20 22:52:04 -0300

    df022.py
    Crie um programa que leia o nome completo de uma pessoa e mostre
    O nome com todas as letras maiúsculas
    O nome com todas as letras minuscula
    Quantas letras ao todo sem considerar espaço
    Quantas letras tem o nome

---
+++ df022.py
@@ -0,0 +1,11 @@
+'''
+Crie um programa que leia o nome completo de uma pessoa e mostre
+O nome com todas as letras maiúsculas
+O nome com todas as letras minuscula
+Quantas letras ao todo sem considerar espaço
+Quantas letras tem o nome
+'''
+nome = input(' Digite seu nome completo ')
+print(nome.upper())
+print(nome.lower())
+print(len(nome))
