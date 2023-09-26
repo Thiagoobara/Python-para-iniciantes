986e83281db0208d1f62f6b1ba24be3f826ea2e3
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-26 08:44:06 -0300

    df023.py
    Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos dígitos separados.
    Ex:
    Digite um numero : 1834
    
    unidades : 4
    dezena : 3
    centena : 8
    milhar : 1

--- df023.py
+++ df023.py
@@ -8,3 +8,15 @@ dezena : 3
 centena : 8
 milhar : 1
 '''
+#// divisão inteira
+#% resto da divisão
+nu = int(input('Digite um número de 0 a 9999 '))
+uni = nu // 1 % 10
+de = nu // 10 % 10
+cent = nu // 100 % 10
+mil = nu // 1000 % 10
+print('Valor em uni {}'.format(uni))
+print('Valor da dez {}'.format(de))
+print('Valor da cent {}'.format(cent))
+print('Valor de mil {}'.format(mil))
+
