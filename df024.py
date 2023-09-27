7e0c0c8cbcb6fa396b8ffca95199cea1e57bbe04
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-26 23:35:42 -0300

    df024.py
     Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo'

--- df024.py
+++ df024.py
@@ -1,7 +1,7 @@
 '''
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo'
 '''
-nome = str(input("Digite o nome da cidade ")).strip.lower()
+nome = str(input("Digite o nome da cidade ")).strip().lower()
 print(nome[:5] == 'santo')
 
 
