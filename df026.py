afde1db027410c13c2c74b75e9953c0c95a37836
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-09-26 23:35:20 -0300

    df026.py
    Faça um programa que leia uma frase pelo teclado e mostre
    quantas vezes aparece a letra 'A'
    em que posição ela aparece a 1°
    em que posição ela aparece a ultima vez

---
+++ df026.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que leia uma frase pelo teclado e mostre
+quantas vezes aparece a letra 'A'
+em que posição ela aparece a 1° 
+em que posição ela aparece a ultima vez  
+'''
+
+frase = str(input('Digite uma frase ')).lower()
+print(frase.count('a'))
