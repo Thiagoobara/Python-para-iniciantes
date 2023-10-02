ffd1ef74881f245d342844392fb9d56eaba0e395
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-10-02 08:20:02 -0300

    df029.py
    Escreva um programa que leia a velocidade de um carro .
    se ele ultrapassar 80km , mostre a mensagem dizendo que ele foi multado
    a multa vai custar R$7,00 a cada km a cima

--- df029.py
+++ df029.py
@@ -3,16 +3,21 @@ Escreva um programa que leia a velocidade de um carro .
 se ele ultrapassar 80km , mostre a mensagem dizendo que ele foi multado 
 a multa vai custar R$7,00 a cada km a cima 
 '''
-
+import time
 vel = float(input('Digite a velocidade do carro '))
-limite = 80
-multa = vel - limite
-pag = multa * 7
-if limite < 0:
-    print('Não teve multa ')
-else:
-    print('Voce tera que pagar {:.2f} de multa pois estava a {:.2f} km por hora'.format(pag, vel ))
+if vel >80:
+    print('MULTADO!!!! Você excedeu a limite permitido')
+    multa = (vel - 80) * 7
+    time.sleep(1)
+    print('Calculando o valor da multa aguarde ...')
+    time.sleep(1)
+    print('Sua multa foi de R${:.2f} diriga com responsabilidade! '.format(multa))
+print('Boa viagem!!!! ')
 
+    
+
+
+    
 
 
 
