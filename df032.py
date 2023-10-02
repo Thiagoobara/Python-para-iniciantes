75d67a79e472a477051f245cb9cb649153899415
Thiago Felipe Obara <thiago.obara@yahoo.com.br>
2023-10-02 08:20:20 -0300

    df032.py
    Faça um programa que mostre se o ano é bissexto

--- df032.py
+++ df032.py
@@ -2,9 +2,9 @@
 Faça um programa que mostre se o ano é bissexto
 '''
 ano = int(input('Digite o ano '))
-bi = ano % 4
-if bi == 0:
-    print('Esse ano é bissexto ')
+bi = ano 
+if ano % 4 == 0 and ano % 100 !=0 or ano % 400 == 0:
+    print('Esse ano é bissexto  ')
 else:
     print('Esse ano não é bissexto ')
 print('---FIM---')
